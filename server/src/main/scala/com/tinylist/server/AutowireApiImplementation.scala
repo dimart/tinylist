88d128b3cb1ee9839099ef280b05ae386caab6ba
Dmitrii Petukhov <dimart.sp@gmail.com>
2017-03-30 07:47:42 +0200

    server: impl fetch (save in-memory for now)

--- server/src/main/scala/com/tinylist/server/AutowireApiImplementation.scala
+++ server/src/main/scala/com/tinylist/server/AutowireApiImplementation.scala
@@ -3,24 +3,27 @@ package com.tinylist.server
 import java.nio.ByteBuffer
 import java.util.{Base64, UUID}
 
-import com.tinylist.api.{AutowireApi, TinyList, TinyListId}
+import com.tinylist.api.{AutowireApi, TinyList, TinyListId, TinyLists}
 
 class AutowireApiImplementation extends AutowireApi {
-  private var savedLists = Seq[TinyList]()
+  private var savedLists = Map[TinyListId, TinyList]()
 
   def save(tinyList: TinyList): TinyListId = {
-    savedLists :+= tinyList
     val uuid = randomUrlFirendlyBase64UUID
-    TinyListId(uuid)
+    val res = TinyListId(uuid)
+    savedLists = savedLists.updated(res, tinyList)
+    res
   }
 
 
-  def fetch(tinyListId: TinyListId): TinyList = ???
+  def fetch(tinyListId: TinyListId): TinyList = {
+    savedLists.getOrElse(tinyListId, TinyLists.default)
+  }
 
   /*
-      Thanks to:
-      https://github.com/scalacenter/scastie/blob/master/balancer/src/main/scala/com.olegych.scastie.balancer/SnippetsContainer.scala#L259
-     */
+    Thanks to:
+    https://github.com/scalacenter/scastie/blob/master/balancer/src/main/scala/com.olegych.scastie.balancer/SnippetsContainer.scala#L259
+   */
   private def randomUrlFirendlyBase64UUID: String = {
     def toBase64(uuid: UUID): String = {
       val (high, low) =
